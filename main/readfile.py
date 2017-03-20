876656170f11a3aef438ff0439955fccdd7d6928
liuluyang <1120773382@qq.com>
2017-03-20 10:43:12 +0800

    add class readfile

--- main/readfile.py
+++ main/readfile.py
@@ -1,3 +1,18 @@
 #coding:utf8
 
 
+import os
+
+
+class Readfile(object):
+
+    def __init__(self):
+        pass
+
+    def readfirst(self):
+        print 'start read'
+
+
+def __name__ == '__main__':
+    Readfile()
+
